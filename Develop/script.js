50b09fb4fd9cea1aa24c20001914baf2bf4c147f
Aubrey McKinney <99014955+Shadowasders@users.noreply.github.com>
2023-04-20 18:48:18 -0400

    Added fucntionality for saving text in blocks, working out loading them

--- Develop/script.js
+++ Develop/script.js
@@ -6,7 +6,6 @@ var currentDayEl = $("#currentDay");
 var saveButtonEl = $(".btn");
 var timeBlockEL = $(".description")
 var existingActivitiesArray = JSON.parse(localStorage.getItem('savedActivities')) || [];
-
 $(function () {
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
@@ -16,23 +15,32 @@ $(function () {
   // useful when saving the description in local storage?
   saveButtonEl.on("click", function(){
     var parentDiv = $(this).parents('div.time-block');
-    var timeBlock = $(parentDiv).attr('id'); console.log(timeBlock);
+    var timeBlock = $(parentDiv).attr('id');
     var textareaEl = $(parentDiv).children()[1];
     var textToSave = $(textareaEl).val();
     var savedText = {
-      //timeBlock, // timeBlock: timeBlock
-      //textToSave // textToSave: textToSave
       timeBlock: timeBlock,
       savedText: textToSave
     }
-
-    // filter for the item in the existingActivitiesArray with the timeBlock value
-    // of our current object savedText
-    existingActivitiesArray.length == 0 ? existingActivitiesArray.push(savedText) : (
-      existingActivitiesArray.findIndex()
-    )
-    console.log('texttosave', textToSave);
+    existingActivitiesArray.push(savedText)
+    // existingActivitiesArray.length == 0 ? existingActivitiesArray.push(savedText) : (
+    //   existingActivitiesArray.findIndex(savedActivites)
+    // )
+localStorage.setItem("savedActivites", JSON.stringify(existingActivitiesArray));
   })
+function init() {
+  var loadedTime = JSON.parse(localStorage.getItem(existingActivitiesArray.timeBlock));
+  var loadedText = JSON.parse(localStorage.getItem(existingActivitiesArray.textToSave));
+    for (var i = 0; i < existingActivitiesArray.length; i++){
+      var hours = [9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21];
+      if (hours === existingActivitiesArray[i].timeBlock){
+        $("#hour- " + [i]).text(existingActivitiesArray[i].text)
+    }
+  } 
+}
+
+
+
 
   // TODO: Add code to apply the past, present, or future class to each time
   // block by comparing the id to the current hour. HINTS: How can the id
@@ -40,18 +48,13 @@ $(function () {
   // past, present, and future classes? How can Day.js be used to get the
   // current hour in 24-hour time?
   var currentTime = parseInt(dayjs().format("HH"));
-  console.log(typeof currentTime);
-  
   var times = [9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21];
   for (var i = 0; i < times.length; i++) {
     if (times[i] < currentTime) {
-      console.log('1');
       $("#hour-" + times[i] + " textarea").addClass("past");
     } else if (times[i] === currentTime) {
-      console.log('2');
       $("#hour-" + times[i] + " textarea").addClass("present");
     } else if (times[i] > currentTime) {
-      console.log('3');
       $("#hour-" + times[i] + " textarea").addClass("future");
     }
   };
@@ -59,8 +62,8 @@ $(function () {
   // TODO: Add code to get any user input that was saved in localStorage and set
   // the values of the corresponding textarea elements. HINT: How can the id
   // attribute of each time-block be used to do this?
-  //
-  // TODO: Add code to display the current date in the header of the page.
 
+  // TODO: Add code to display the current date in the header of the page.
+  init();
   $(currentDayEl).text(localTime.format("dddd, MMMM DD hh:mm A"));
 });
